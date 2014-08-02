9ded08adac3e1a59ab7addc5265b3a844e0ef788
Julie Behr <julie.behr@choderalab.org>
2014-08-02 16:50:35 -0400

    added element correction for topology

--- src/density_sim.py
+++ src/density_sim.py
@@ -9,6 +9,8 @@ from gaff2xml.utils import create_ffxml_file
 from gaff2xml.utils import convert_molecule
 import mdtraj as md
 import numpy as np
+import simtk.openmm.app.element as elem
+import pymbar.timeseries as ts
 
 
 def case(molecule_name, num, steps, ff_mod):
@@ -27,7 +29,17 @@ def case(molecule_name, num, steps, ff_mod):
     traj, xyz, top = set_topology(infile, num)
     system, integrator, temperature = openmm_system(forcefield, top)
     openmm_simulation(system, top, integrator, xyz, temperature, outfile, lastframe_file, outdata, dcd_file, steps)
-    avg_density(dcd_file,lastframe_file)
+    avg_density(dcd_file, lastframe_file, outdata)
+
+def massmod_case(molecule_name, num, steps, ff_mod):
+    infile, topfile, outfile, outdata, lastframe_file, dcd_file, mol2_filename = name_outputs(molecule_name, num, ff_mod)
+    add_bonds_to_pdb(infile, topfile)
+    forcefield = generate_ff(molecule_name, infile, mol2_filename, ff_mod, topfile)
+    traj, xyz, top = set_topology(infile, num)
+    system, integrator, temperature = openmm_system(forcefield, top)
+    system = modify_mas(system, traj, topfile)
+    openmm_simulation(system, top, integrator, xyz, temperature, outfile, lastframe_file, outdata, dcd_file, steps)
+    avg_density(dcd_file, lastframe_file, outdata)
 
 def name_outputs(molecule_name, num, ff_mod):
     infile = molecule_name+'.pdb'
@@ -78,6 +90,7 @@ def set_topology(infile, num):
     traj = gaff2xml.packmol.pack_box([infile],[num])
     xyz = traj.openmm_positions(0)
     top = traj.top.to_openmm()
+    top = assign_element(top)
     top.setUnitCellDimensions(mm.Vec3(*traj.unitcell_lengths[0])*u.nanometer)
     return traj, xyz, top
 
@@ -102,31 +115,62 @@ def openmm_simulation(system, top, integrator, xyz, temperature, outfile, lastfr
     simulation.reporters.append(app.DCDReporter(dcd_file,1000))
     simulation.step(steps)
 
-def avg_density(dcd_file,lastframe_file):
+def avg_density(dcd_file,lastframe_file, outdata):
     trj = md.load(dcd_file, top=lastframe_file)
-    volume = trj.unitcell_lengths.prod(1) * u.nanometer**3
-    mass = sum([a.element.mass for a in trj.top.atoms]) * u.daltons
-    mass = mass * u.mole / 6.0221413e23
-    mass = mass.in_units_of(u.gram)
-    density = mass / volume
-    avg_density = density.mean()
-    avg_density = avg_density * u.gram
-    avg_density = avg_density.in_units_of(u.gram / u.liter)
-    density_file = 'density_'+dcd_file[:-4]+'.dat'
+    volume = trj.unitcell_lengths.prod(1)
+    mass = sum([a.element.mass for a in trj.top.atoms]) / 6.0221413e23
+    density_nounit = mass / volume
+    density = density_nounit * u.gram / u.nanometer**3
+    A_t = np.array(density_nounit )
+    indices = ts.subsampleCorrelatedData(A_t)
+    ind_density = density[indices]
+    avg_ind_density = ind_density.mean().in_units_of(u.gram / u.liter)
+    std_ind_density = ind_density.std().in_units_of(u.gram / u.liter)
+    N = len(indices)
+    stderr_ind_density = std_ind_density / (N**0.5)
+    temps = []
+    fid = open(outdata, 'r')
+    fid.next()
+    for line in fid:
+        dtemp = float(line.split(',')[1])
+        temps.append(dtemp)
+    fid.close()
+    temps = np.array(temps)
+    avg_temp = temps.mean()
+    density_file = 'density_'+dcd_file[:-4]+'_indstd.dat'
     f = open(density_file, 'w')
     f.write("Average density of the system:\n")
-    f.write(str(avg_density))
+    f.write(str(avg_ind_density))
+    f.write("\nStandard Deviation of density:\n")
+    f.write(str(std_ind_density))
+    f.write("\nStandard Error of the density:\n")
+    f.write(str(stderr_ind_density))
+    f.write("\nAverage Temperature of the system:\n")
+    f.write(str(avg_temp))
+    f.close()
 
-def modify_mass(system, traj):
+def modify_mass(system, traj, topfile):
     n_atoms = traj.n_atoms
     n_residues = traj.n_residues
     mlc_atoms = n_atoms / n_residues
     gtop = app.GromacsTopFile(topfile)._currentMoleculeType
     lock = dict((row[4],float(row[7])) for row in gtop.atoms)
+    print("\nModified atomic masses:")
     for i in range(mlc_atoms):
         mass = lock[traj.top.atom(i).name]
         for x in np.linspace(i, n_atoms+i, n_residues, endpoint=False):
             system.setParticleMass(int(x), mass *u.dalton)
+        print(str(traj.top.atom(i).name)+' '+str(system.getParticleMass(i)))
+    print("\n##############################################################\n")
+    return system
+
+def assign_element(top):
+    for chain in top.chains():
+        for res in chain.residues():
+            for atom in res.atoms():
+                ## this is only going to work as long as there are fewer than 10 of an atom type
+                atom.element = elem.get_by_symbol(atom.name[:-1])
+    return top
 
 if __name__ == '__main__':
     molecule_name = raw_input('Name of molecule\n')
